857dca3da1245a0a69abfe414946f4593231a6ac
unknown <Peter1031734590@gmail.com>
2023-11-23 22:39:06 +1000

    changed file structure and edited gitignore file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React TOOOO
         </a>
       </header>
     </div>
